db40af099de2d52df7e65de616f0843d4771b221
UzairKhan313 <uzairkhaan2003@gmail.com>
2025-06-27 17:07:32 +0500

    fix: fixing husky error

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,14 @@
 export default function Home() {
-	return (
-		<div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
-			<main className="row-start-2 flex flex-col items-center gap-[32px] sm:items-start">
-				<h1>Hello world</h1>
-				<ol className="list-inside list-decimal text-center font-[family-name:var(--font-geist-mono)] text-sm/6 sm:text-left">
-					<li className="mb-2 tracking-[-.01em]">
-						Next Js With Tailwind Css Starter .
-					</li>
-					<li className="tracking-[-.01em]">
-						Save and see your changes instantly.
-					</li>
-				</ol>
-			</main>
-		</div>
-	);
+  return (
+    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
+      <main className="row-start-2 flex flex-col items-center gap-[32px] sm:items-start">
+        <h1>Hello world</h1>
+
+        <ol className="list-inside list-decimal text-center font-[family-name:var(--font-geist-mono)] text-sm/6 sm:text-left">
+          <li className="mb-2 tracking-[-.01em]">Next Js With Tailwind Css Starter .</li>
+          <li className="tracking-[-.01em]">Save and see your changes instantly.</li>
+        </ol>
+      </main>
+    </div>
+  );
 }
